510a68d72bf6bcf1a378b53a3d657dee21fa42c7
NoemitoJohn <129826378+NoemitoJohn@users.noreply.github.com>
2023-09-09 00:01:45 +0800

    card

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.send("Home Page")
 
+app.get('/', (req, res) => { // http://127.0.0.1:3000/ = /
+    res.render('index', {packages: [{name: 'package1'}, {name: 'package2' }, {name: 'package3'}, {name: 'package4'}]})
 })
+
+
 
 app.get('/booking', (req, res) => {
     res.send("Booking Page")
